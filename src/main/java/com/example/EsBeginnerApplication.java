3929bba2971076e1e4eb855a90c8502e92fdcb2b
rakus-inadayutaka0707 <yutaka.inada@rakus-partners.co.jp>
2021-08-04 10:46:23 +0900

    first commit.

---
+++ src/main/java/com/example/EsBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsBeginnerApplication.class, args);
+	}
+
+}
