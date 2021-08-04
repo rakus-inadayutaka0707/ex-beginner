b2c54a04075c7b2325d80ca531658f15e70a1c89
rakus-inadayutaka0707 <yutaka.inada@rakus-partners.co.jp>
2021-08-05 06:51:49 +0900

    gitがうまく動かせなかったので再度commit.

--- src/main/java/com/example/controller/ShoppingCartController.java
+++ src/main/java/com/example/controller/ShoppingCartController.java
@@ -29,48 +29,21 @@ public class ShoppingCartController {
 		List<Item> itemList = (List<Item>) application.getAttribute("itemList");
 		if (itemList == null) {
 			itemList = new LinkedList<>();
-<<<<<<< HEAD
 			itemList.add(new Item("手帳ノート", 1000));
 			itemList.add(new Item("文房具セット", 1500));
 			itemList.add(new Item("ファイル", 2000));
-=======
-			Item item1 = new Item();
-			item1.setName("手帳ノート");
-			item1.setPrice(1000);
-			itemList.add(item1);
-
-			Item item2 = new Item();
-			item2.setName("文房具セット");
-			item2.setPrice(1500);
-			itemList.add(item2);
-
-			Item item3 = new Item();
-			item3.setName("ファイル");
-			item3.setPrice(2000);
-			itemList.add(item3);
->>>>>>> 17fe445e7d2cc63ae7dd197a9ce8869c21ad7c90
 
 			application.setAttribute("itemList", itemList);
 		}
 		int totalPrice = 0;
-<<<<<<< HEAD
 		if(session.getAttribute("cartItemList") != null) {
 			@SuppressWarnings("unchecked")
 			List<Item> cartItem = (List<Item>) session.getAttribute("cartItemList");
 			for (Item item : cartItem) {
-=======
-		@SuppressWarnings("unchecked")
-		List<Item> cartItemList = (List<Item>) session.getAttribute("cartItemList");
-		if (cartItemList == null) {
-
-		} else {
-			for (Item item : cartItemList) {
->>>>>>> 17fe445e7d2cc63ae7dd197a9ce8869c21ad7c90
 				totalPrice += item.getPrice();
 			}
 		}
 		model.addAttribute("totalPrice", totalPrice);
-<<<<<<< HEAD
 		return "item-and-cart";
 	}
 	
@@ -85,45 +58,21 @@ public class ShoppingCartController {
 			for(Item item:cartItem) {
 				cartItemList.add(item);
 			}
-=======
-		return "exam6/item-and-cart";
-	}
-
-	@SuppressWarnings("unchecked")
-	@RequestMapping("/in-cart")
-	public String inCart(int indexNum) {
-		List<Item> itemList = (List<Item>) application.getAttribute("itemList");
-		List<Item> cartItemList = new LinkedList<>();
-		if (session.getAttribute("cartItemList") != null) {
-			cartItemList = (List<Item>) session.getAttribute("cartItemList");
->>>>>>> 17fe445e7d2cc63ae7dd197a9ce8869c21ad7c90
 		}
 		cartItemList.add(itemList.get(indexNum));
 		session.setAttribute("cartItemList", cartItemList);
 		return "redirect:/exam6/redirect";
 	}
-<<<<<<< HEAD
 	
 	@RequestMapping("/delete")
 	public String delete(int indexNum) {
 		@SuppressWarnings("unchecked")
 		List<Item> cartItemList = (List<Item>)session.getAttribute("cartItemList");
-=======
-
-	@RequestMapping("/out-cart")
-	public String outCart(int indexNum) {
-		@SuppressWarnings("unchecked")
-		List<Item> cartItemList = (List<Item>) session.getAttribute("cartItemList");
->>>>>>> 17fe445e7d2cc63ae7dd197a9ce8869c21ad7c90
 		cartItemList.remove(indexNum);
 		session.setAttribute("cartItemList", cartItemList);
 		return "redirect:/exam6/redirect";
 	}
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> 17fe445e7d2cc63ae7dd197a9ce8869c21ad7c90
 	@RequestMapping("/redirect")
 	public String redirect(Model model) {
 		return index(model);
--- src/main/java/com/example/domain/Item.java
+++ src/main/java/com/example/domain/Item.java
@@ -3,7 +3,6 @@ package com.example.domain;
 public class Item {
 	private String name;
 	private Integer price;
-<<<<<<< HEAD
 	
 	public Item() {
 	}
@@ -13,8 +12,6 @@ public class Item {
 		this.name = name;
 		this.price = price;
 	}
-=======
->>>>>>> 17fe445e7d2cc63ae7dd197a9ce8869c21ad7c90
 
 	public String getName() {
 		return name;
